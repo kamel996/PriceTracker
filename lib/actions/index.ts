95ba53f046a3a5f158ac17e0c873302b035d7e0f
kamel996 <kamelnassar2828@gmail.com>
2023-11-21 11:13:49 +0200

    setting mongo db connection

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,11 +1,13 @@
 "use server"
 
 import {scrapeAmazonProduct} from "@/lib/scraper";
+import {connectToDB} from "@/lib/mongoose";
 
 export async function scrapeAndStoreProduct(productUrl: string){
     if(!productUrl) return;
 
     try {
+        connectToDB();
         const scrapedProduct = await scrapeAmazonProduct(productUrl)
         if (!scrapedProduct) return ;
 
